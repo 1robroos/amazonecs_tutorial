17f4d8bc8a43199da4a4a2fb876bc068242c5a54
Rob Roos <1robroos@gmail.com>
2019-01-05 10:44:07 +0100

    change region

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -t tetranoodle:v_$BUILD_NUMBER --pull=true /var/lib/jenkins/workspace/tetranoodle-app \
-&& docker tag tetranoodle:v_$BUILD_NUMBER 969526043371.dkr.ecr.us-east-1.amazonaws.com/tetranoodle:v_$BUILD_NUMBER \
-&& docker push 969526043371.dkr.ecr.us-east-1.amazonaws.com/tetranoodle:v_$BUILD_NUMBER
+&& docker tag tetranoodle:v_$BUILD_NUMBER 969526043371.dkr.ecr.eu-west-2.amazonaws.com/tetranoodle:v_$BUILD_NUMBER \
+&& docker push 969526043371.dkr.ecr.eu-west-2.amazonaws.com/tetranoodle:v_$BUILD_NUMBER
 
 
 
